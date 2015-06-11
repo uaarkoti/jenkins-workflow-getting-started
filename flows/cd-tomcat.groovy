513115aacb9ba2008e61d439a936109d263c788a
Udaypal Aarkoti <uaarkoti@gmail.com>
2015-06-11 10:48:59 -0700

    Added color commentary

--- flows/cd-tomcat.groovy
+++ flows/cd-tomcat.groovy
@@ -1,17 +1,22 @@
 // Global Libraries
+
+// Tomcat library to deploy / undeploy to tomcat
 tomcat = new com.cb.web.Tomcat(hostname: "localhost", port: "8180", adminUser: "admin", adminPassword: "tomcat")
+
+// Simple utility
 util = new com.cb.util.BasicUtilities()
 
 // Local variables
 artifactName = 'webapp.war'
 artifact = "target/${artifactName}"
 
-// Closures to be executed by tomcat libraries
+// Closures to be executed by tomcat library to deploy/undeploy
 deployClosure = {war, url, id -> sh "curl --upload-file ${war} '${url}?path=/${id}&update=true'"}
 undeployClosure = {url, id -> sh "curl '${url}?path=/${id}'"}
 deployClosure.resolveStrategy = Closure.DELEGATE_FIRST
 undeployClosure.resolveStrategy = Closure.DELEGATE_FIRST
 
+// Execute the following steps on the master
 node('master') {
    git url: 'https://github.com/jenkinsbyexample/workflow-plugin-pipeline-demo.git'
    devQAStaging()
@@ -21,13 +26,16 @@ production()
 
 def devQAStaging() {
 
+    // Execute maven build and archive artifacts
     stage 'Build'
     sh 'mvn clean package'
     archive artifact
 
+    // TODO : Setup code coverage
     stage 'Code Coverage'
     echo 'Using Sonar for code coverage'
 
+    // Run tests in parallel and publish report
     stage 'QA'
 
     parallel(longerTests: {
@@ -39,19 +47,34 @@ def devQAStaging() {
             sh "mvn -f sometests/pom.xml test -Durl=${url} -Dduration=5"
         }
     })
+ 
+    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 
+    // Assuming the tests above take a while (which is probably true in real world)
+    // Setup a checkpoint so that if the build fails after the checkpoint, its possible
+    // to restart the build from the checkpoint
+    // NOTE: The try/catch block makes sure the build does not fail if checkpoint functionality
+    // doesn't exist (because its available through CloudBees Jenkins Enterprise)
     try {
         checkpoint('Before Staging')
     } catch (NoSuchMethodError _) {
         echo 'Checkpoint feature available in Jenkins Enterprise by CloudBees.'
     }
 
+    // Make sure only one build can enter this stage
     stage name: 'Staging', concurrency: 1
+
+    // Deploy the artifact to Tomcat
     tomcat.deploy(artifact, 'staging', deployClosure)
 }
 
 def production() {
+
+    // Wait for someone to validate that the deployment looks good.
+    // Its also possible to add security here to make sure only people
+    // With a specific role can resume the build from here
     input message: "Does ${tomcat.hostUrl}/staging/ look good?"
+
     try {
         checkpoint('Before production')
     } catch (NoSuchMethodError _) {
@@ -67,6 +90,7 @@ def production() {
     }
 }
 
+// Simple utility function to run tests on tomcat at random url
 def runWithServer(body) {
     def id = util.random()
     tomcat.deploy(artifact, id, deployClosure)
